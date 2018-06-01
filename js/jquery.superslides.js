295684fc9bde07d29e8d5d07b7d8ec4b198e6e1d
xiaosizhe <xiaosizheme@gmail.com>
2018-06-01 21:02:38 +0800

    change time

--- js/jquery.superslides.js
+++ js/jquery.superslides.js
@@ -8,7 +8,7 @@ var Superslides, plugin = 'superslides';
 Superslides = function(el, options) {
   this.options = $.extend({
     play: false,
-    animation_speed: 2000,
+    animation_speed: 10000,
     animation_easing: 'swing',
     animation: 'slide',
     inherit_width_from: window,
